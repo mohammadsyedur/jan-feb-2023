d2b2f9df71790443953300d8ba116093d280e71c
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2023-01-23 10:50:07 +0530

    Day01

---
+++ day01/src/main/java/com/herbalife/Hello.java
@@ -0,0 +1,18 @@
+//All package names begin with lowercase
+package com.herbalife;
+
+//camelCase notation is followed in Java
+//All classnames begin with uppercase
+public class Hello {
+    //All method names begin with lowercase unlike C#
+    public static void main(String[] args) {
+        System.out.println("Hello in IntelliJ");
+        System.out.println("sout is the code snippet for println statement in IntelliJ");
+    }
+
+    static void tellMeYourName() {
+        /*
+        * Multi line comments in Java
+        * */
+    }
+}
